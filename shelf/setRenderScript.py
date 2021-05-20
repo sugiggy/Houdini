b6538ef6872f2c7139b2deb7c86464b944f118b7
Masaya Sugimura <sugimura_m@yahoo.co.jp>
2021-05-19 21:55:34 -0400

    Update setRenderScript.py

--- shelf/setRenderScript.py
+++ shelf/setRenderScript.py
@@ -1,11 +1,12 @@
 #file = '$LAUNCHER_LIB/houdini/Pre-Render_Script/keepMasterFile.py'
 #node.parm('prerender').set('exec(compile(open('+ file +').read(),'+file+', "exec"))' )
 
+pre = 'execfile("$LAUNCHER_LIB/houdini/renderScripts/PreRender.py")'
+post = 'execfile("$LAUNCHER_LIB/houdini/renderScripts/PostRender.py")'
+
 for node in hou.selectedNodes():
     orgPre = node.parm('prerender').eval()
     orgPost = node.parm('postrender').eval()
-    pre = 'execfile("$LAUNCHER_LIB/houdini/renderScripts/PreRender.py")'
-    post = 'execfile("$LAUNCHER_LIB/houdini/renderScripts/PostRender.py")'
     if len(orgPre)>0:
         pre = orgPre+ '\n' + pre
     node.parm('prerender').set(pre)
@@ -14,3 +15,4 @@ for node in hou.selectedNodes():
         post = orgPost + '\n' + post
     node.parm('postrender').set(post)
     node.parm('lpostrender').set('python')
+
