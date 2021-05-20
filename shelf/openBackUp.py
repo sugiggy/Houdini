70817ab8095d95ee3c6d355effa9a4fb0f4a7f91
Masaya Sugimura <sugimura_m@yahoo.co.jp>
2021-05-19 22:32:20 -0400

    Create openBackUp.py

---
+++ shelf/openBackUp.py
@@ -0,0 +1,18 @@
+import hou
+import os
+
+#username = getpass.getuser()
+
+#dir = '/mnt/LOCAL/TMP/'
+#print dir
+dir = hou.getenv("HOUDINI_TEMP_DIR")
+print dir
+files = []
+for file in os.listdir(dir):
+    if file.endswith(".hiplc"):
+        files.append(dir+file)
+newest = max(files,key=os.path.getctime)
+#print newest
+
+hou.setUpdateMode(hou.updateMode.Manual)
+hou.hipFile.load(newest)
