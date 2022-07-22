1082c4fe76daed43e3c333797e5791ced95a4e56
Masaya Sugimura <sugimura_m@yahoo.co.jp>
2022-07-22 18:24:05 -0400

    Update sopToLop.py

--- shelf/sopToLop.py
+++ shelf/sopToLop.py
@@ -1,13 +1,37 @@
 stage = hou.node("/stage")
-num = 0
+numA = 0
+parm_prefix = '_reference'
 
 for node in hou.selectedNodes():
     curParent = node.parent().name()
-    num += 1
+    numA += 1
     
     sopImport = stage.createNode("sopimport")
     sopImport.parm("soppath").set(node.path())
+    
+    #################
+    group = node.parmTemplateGroup()
+    
+    ## remove parms
+    parms = group.entries()
+    for p in parms:
+        if p.name().startswith(parm_prefix): group.remove(p)
+    
+    ## get refs
+    node.setParmTemplateGroup( group )
+    
+    depNodes = node.dependents()
+    num = 0
+    for depNode in depNodes:
+        parmName = parm_prefix+str(num)
+        num += 1
+        
+        parm = hou.StringParmTemplate(parmName, "",1,string_type=hou.stringParmType.NodeReference)
+        path = depNode.path()
+        node.addSpareParmTuple(parm)
+        node.parm(parmName).set(path)
+    ####################   
         
     p = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
     p.setCurrentNode(sopImport)
-    #p.homeToSelection()
+    p.homeToSelection()
