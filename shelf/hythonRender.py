74fe50d4664cd57240ca30caccfbd893ca10e2af
Masaya Sugimura <sugimura_m@yahoo.co.jp>
2022-08-14 20:18:51 -0400

    Create hythonRender.py

---
+++ shelf/hythonRender.py
@@ -0,0 +1,24 @@
+import os
+import hou
+import platform
+import subprocess
+from subprocess import Popen
+
+#node = kwargs['node']
+L_LIB = hou.getenv("LAUNCHER_LIB")
+
+myFile = hou.hipFile.path()
+print('saved hip file')
+hou.hipFile.save()
+
+if platform.system()=='Linux':
+    py = '/mnt/NAS/CG/pipeline/launcher_0.4.0/launcherlib/hythonRender.py'
+    if 'PYTHONHOME' in os.environ:
+        del os.environ['PYTHONHOME']
+    command = 'hython '+ py +' ' + myFile +  " &"
+    subprocess.call(['/bin/bash', '-i', '-c', command])
+
+if platform.system()=='Windows':
+    py = 'Z:\\CG\pipeline\\launcher_0.4.0\\launcherlib\\hythonRender.py'
+    command = 'hython '+ py +' ' + myFile +  " &"
+    subprocess.Popen(command.split())
