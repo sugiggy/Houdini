3e9dc62fc857a6256f5c6766e72b71d8895f8a75
Masaya Sugimura <sugimura_m@yahoo.co.jp>
2022-08-22 19:56:43 -0400

    Update hythonRender.py

--- pipeline/launcher_0.4.0/launcherlib/hythonRender.py
+++ pipeline/launcher_0.4.0/launcherlib/hythonRender.py
@@ -37,27 +37,28 @@ for i in range(1,len(argvs)):
 ### render each rop
     for Ren in SortedRenderROPs:
         hou.hipFile.load(myFile)
+
         ROPs =  hou.node("/out").children()
         NodeName = Ren[1]
 
         for Rop in ROPs:
             if Rop.name() == NodeName:
+                Rop.bypass(0)
                 #frameText = Rop.render(ignore_inputs=True,verbose=False)
                 #print Rop
                 Rop.setSelected(True, clear_all_selected=True)
                 Rop.setCurrent(True, clear_all_selected=True)
-                if Rop.type().name()!='shell' :
+                if Rop.type().name()!='shell' and Rop.type().name()!='subnet' :  ## subnet should change once I made printCookTime HDA
                     exec(open(L_LIB+"/houdini/renderScripts/PreRender.py").read())
                 #Rop.render(ignore_inputs=True)
 
-                ins = Rop.inputConnections()
-                for inCon in ins:
-                    inIndex = inCon.inputIndex()
-                    inNode = inCon.outputNode()
-                    inNode.setInput(inIndex,None,0)
+                ##Bypass all rops except Current Rop
+                for node in ROPs:
+                    if node.name() != NodeName: node.bypass(1)
 
                 Rop.parm('execute').pressButton()
-                if Rop.type().name()!='shell' :
+
+                if Rop.type().name()!='shell' and Rop.type().name()!='subnet' :  ## subnet should change once I made printCookTime HDA
                     exec(open(L_LIB+"/houdini/renderScripts/PostRender.py").read())
 
         #frameText = hou.hscript("render -s -V "+RenName)
