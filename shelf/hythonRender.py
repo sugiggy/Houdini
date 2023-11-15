d11b72304c708535e6ae2a2d7ba807937789c33d
Masaya Sugimura <sugimura_m@yahoo.co.jp>
2023-11-15 16:23:33 -0500

    Update hythonRender.py

--- shelf/hythonRender.py
+++ shelf/hythonRender.py
@@ -1,25 +1,33 @@
 import os
+import subprocess
 import hou
 import platform
-import subprocess
 from subprocess import Popen
 
-#node = kwargs['node']
-L_LIB = hou.getenv("LAUNCHER_LIB")
-
-myFile = hou.hipFile.path()
-print('saved hip file')
 hou.hipFile.save()
+myFile = hou.hipFile.path()
+os.chdir(hou.getenv('HIP'))
+
+hou.hipFile.save()
+print('hip file saved')
 
 """
 if platform.system()=='Linux':
     py = '/mnt/NAS/CG/pipeline/launcher_0.4.0/launcherlib/hythonRender.py'
+    command = 'hython '+ py +' ' + myFile
     if 'PYTHONHOME' in os.environ:
         del os.environ['PYTHONHOME']
-    command = 'hython '+ py +' ' + myFile
     subprocess.call(['/bin/bash', '-i', '-c', command])
 """
+
 #if platform.system()=='Windows':
-py = hou.getenv("NAS")+'/CG/pipeline/launcher_0.4.0/launcherlib/hythonRender.py'
-command = 'hython '+ py +' ' + myFile
-subprocess.Popen(command.split())
+py = hou.getenv('NAS')+'/CG/pipeline/launcher_0.4.0/launcherlib/hythonRender.py'
+myCommand = 'hython '+ py +' ' + myFile
+#print(myCommand)
+
+if platform.system()=='Windows': 
+    subprocess.Popen(myCommand.split())
+    
+if platform.system()=='Linux':
+    command = "x-terminal-emulator -e " + myCommand 
+    subprocess.run(command, shell=True)
