e031368ae69d58956596575e2ee531bd2237dc6c
Masaya Sugimura <sugimura_m@yahoo.co.jp>
2024-01-23 15:45:18 -0500

    Update addGetParameters.py

--- python/addGetParameters.py
+++ python/addGetParameters.py
@@ -6,6 +6,8 @@ flipParms = ['particlesep','gridscale','veltransfer','surfacetension','default_v
 
 node = kwargs['node']
 camera = hou.node(node.parm('camera').eval())
+targetNode = hou.node(node.parm('targetnode').eval())
+
 try:
     group = camera.parmTemplateGroup()
     folder = hou.FolderParmTemplate('openGL_view', 'OpenGL View')
@@ -19,13 +21,13 @@ except: pass
 comment = camera.parm('vcomment').unexpandedString();
 
 #node = hou.selectedNodes()[0]
-targetNode = hou.node(node.parm('targetnode').eval())
-parms = targetNode.parms()
 
+parms = targetNode.parms()
 for p in parms:
-    if targetNode.type().name().startswith('pyro') or targetNode.type().name().startswith('smoke') or targetNode.type().name().startswith('flip'):
-        if p.name() in commonParms or p.name() in pyroParms or p.name() in flipParms  :
-            comment += p.description() +' : '+ '\`substr(chs("' + p.path() + '"),0,6) \`' +'\n'
-    else: comment += p.description() +' : '+ '\`substr(chs("' + p.path() + '"),0,6) \`' +'\n'
+    if 'folder' not in p.name():
+        if targetNode.type().name().startswith('pyro') or targetNode.type().name().startswith('smoke') or targetNode.type().name().startswith('flip'):
+            if p.name() in commonParms or p.name() in pyroParms or p.name() in flipParms  :
+                comment += p.description() +' : '+ '`substr(chs("' + p.path() + '"),0,6) `' +'\n'
+        else: comment += p.description() +' : '+ '`substr(chs("' + p.path() + '"),0,6) `' +'\n'
         
 camera.parm('vcomment').set(comment)
